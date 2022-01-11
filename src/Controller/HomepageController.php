e8f188d798bf1af724a0be7fb683d0d0b6133949
Yassin HAMMOU OUALI <y.hammououali@gmail.com>
2022-01-11 13:58:25 +0100

    initialize project

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function home(): Response
+    {
+        return new Response('Homepage Page!');
+    }
+
+    /**
+     * @Route("/debug")
+     */
+    public function debug(): Response
+    {
+        $object = new \stdClass();
+        $object->title = 'Tintin';
+        $object->createdAt = new \DateTime();
+
+        dump($object);
+
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/contact")
+     */
+    public function contact(): Response
+    {
+        return new Response('Contact Page!');
+    }
+
+    /**
+     * @Route("hello/{firstName<[A-Z a-z]+>}/{lastName<[A-Z a-z]+>}")
+     */
+    public function hello(string $firstName, string $lastName): Response
+    {
+        return new Response(sprintf('Coucou %s %s', $firstName, $lastName));
+    }
+}
